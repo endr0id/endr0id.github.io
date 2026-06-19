297f23d62e9335eb00621b373bd48c4573a61ac9
w4.3nd0 <w4.3nd0@gmail.com>
2026-06-20 02:16:53 +0900

    feat: Setup tailwindcss(#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
