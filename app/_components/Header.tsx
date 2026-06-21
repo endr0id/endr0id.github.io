b6cbb85298b1db1be30d687b63faa05af65d399b
w4.3nd0 <w4.3nd0@gmail.com>
2026-06-21 14:46:37 +0900

    feat: Add dark mode(#3)

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { useTheme } from "next-themes";
+
 const Header = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <div className="flex justify-between items-center px-6 h-14 border-b border-gray-500">
       <div>endr0id deVlog</div>
       <div>
-        <label>dark mode</label>
-        <input type="checkbox" />
+        <button onClick={() => setTheme("light")}>Light Mode</button>
+        <button onClick={() => setTheme("dark")}>Dark Mode</button>
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +27,14 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
-        <Header />
-        {children}
+        <ThemeProvider>
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
