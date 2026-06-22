ffd9ae2a80bed5b85ab8f43146d63836ebd1e4b6
w4.3nd0 <w4.3nd0@gmail.com>
2026-06-23 01:48:17 +0900

    feat: color token設定(#7)
    - border color (light & dark)

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,7 +2,7 @@ import ThemeToggleButton from "./ThemeToggleButton";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center px-6 h-14 border-b border-gray-500">
+    <div className="flex justify-between items-center px-6 h-14 border-b border-outline">
       <h1 className="text-1xl font-semibold">endr0id deVlog</h1>
       <ThemeToggleButton />
     </div>
