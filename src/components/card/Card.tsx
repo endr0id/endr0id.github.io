f5404cdfc78300cbecfe9b6f0326de693b6748a8
w4.3nd0 <w4.3nd0@gmail.com>
2026-06-24 02:49:06 +0900

    feat: タイトルと説明文の改行と折り返し対応(#7)

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -9,13 +9,17 @@ const Card = (props: CardProps) => {
   const { title, description } = props;
 
   return (
-    <div className="flex flex-col gap-2 pb-2 border rounded-lg border-neutral-300 overflow-hidden">
-      <div className="flex items-center justify-center py-2 bg-neutral-200">
+    <div className="flex flex-col gap-2 pb-2 border rounded-lg border-outline overflow-hidden">
+      <div className="flex items-center justify-center py-2 bg-neutral-200 dark:bg-neutral-700">
         <_React size={64} />
       </div>
       <div className="px-6">
-        <h2 className="text-xl font-semibold">{title}</h2>
-        <p className="text-sm text-neutral-500">{description}</p>
+        <h3 className="text-xl whitespace-pre-wrap break-words font-semibold">
+          {title}
+        </h3>
+        <p className="text-sm whitespace-pre-wrap break-words text-neutral-500 dark:text-neutral-300">
+          {description}
+        </p>
       </div>
     </div>
   );
