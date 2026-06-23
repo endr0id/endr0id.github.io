17fa147c9facb95e7b793258761fefdf8a02465f
w4.3nd0 <w4.3nd0@gmail.com>
2026-06-24 02:36:41 +0900

    fix: 適切なエレメントに変更(#7)

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,10 +2,10 @@ import ThemeToggleButton from "./ThemeToggleButton";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center px-6 h-14 border-b border-outline">
+    <header className="flex justify-between items-center px-6 h-14 border-b border-outline">
       <h1 className="text-1xl font-semibold">endr0id deVlog</h1>
       <ThemeToggleButton />
-    </div>
+    </header>
   );
 };
 
