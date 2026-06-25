acbdf08dc07196a9a055f21adf696b230681397e
w4.3nd0 <w4.3nd0@gmail.com>
2026-06-26 00:19:30 +0900

    fix: メタデータとタイトル修正

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,7 +3,7 @@ import ThemeToggleButton from "./ThemeToggleButton";
 const Header = () => {
   return (
     <header className="flex justify-between items-center px-6 h-14 border-b border-outline">
-      <h1 className="text-1xl font-semibold">endr0id deVlog</h1>
+      <h1 className="text-1xl font-semibold">deVlog</h1>
       <ThemeToggleButton />
     </header>
   );
