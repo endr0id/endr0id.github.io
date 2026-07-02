55a9d145d304148a95ced297ab10f9c9d353199f
w4.3nd0 <w4.3nd0@gmail.com>
2026-07-03 02:33:48 +0900

    feat: 見出しの段落毎にインデント挿入(#17)

--- app/blog/_components/toc/TOC.tsx
+++ app/blog/_components/toc/TOC.tsx
@@ -1,4 +1,5 @@
 import type { HeadingInfo } from "@/src/lib/mdx/extractHeading";
+import clsx from "clsx";
 
 type Props = {
   headings: HeadingInfo[];
@@ -6,10 +7,20 @@ type Props = {
 
 const TOC = ({ headings }: Props) => {
   return (
-    <nav className="debug-box">
+    <nav>
       <ul>
         {headings.map((heading) => (
-          <li key={heading.id}>
+          <li
+            key={heading.id}
+            className={clsx("py-1", {
+              "pl-0": heading.level === 1,
+              "pl-4": heading.level === 2,
+              "pl-8": heading.level === 3,
+              "pl-12": heading.level === 4,
+              "pl-16": heading.level === 5,
+              "pl-20": heading.level === 6,
+            })}
+          >
             <a href={`#${heading.id}`}>{heading.title}</a>
           </li>
         ))}
