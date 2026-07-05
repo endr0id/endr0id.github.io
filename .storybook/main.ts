d4341d70f5ed6f796c04bafe3c19ca6470caf65a
w4.3nd0 <w4.3nd0@gmail.com>
2026-07-06 03:05:22 +0900

    chore: 参照箇所追加(#32)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
   stories: [
+    "../app/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
     "../stories/**/*.mdx",
