e21a4b76b1c1da228aa4fe9b938d4bbbb1a976a7
w4.3nd0 <w4.3nd0@gmail.com>
2026-07-17 02:16:11 +0900

    fix: Heading要素のハイライト検知を変更(#23) - viewportの上部20%の領域に到達したheading要素をハイライト対象として検知

--- app/blog/_components/toc/hooks/useActiveHeading.ts
+++ app/blog/_components/toc/hooks/useActiveHeading.ts
@@ -4,14 +4,17 @@ export const useActiveHeading = () => {
   const [activeId, setActiveId] = useState("");
 
   useEffect(() => {
-    // NOTE: スクロールで画面内に表示されたHeadingを現在位置として保持
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setActiveId(entry.target.id);
-        }
-      });
-    });
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveId(entry.target.id);
+          }
+        });
+      },
+      // NOTE: viewportの上部20%の領域のみ交差判定の対象にし、到達したHeadingを現在位置として保持
+      { rootMargin: "0px 0px -80% 0px" },
+    );
 
     // NOTE: 取得したHeadingを監視対象として登録
     document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach((heading) => {
