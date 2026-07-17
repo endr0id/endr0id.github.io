b713c53afd76a0e32205e472efb8206cfeaef8a3
w4.3nd0 <w4.3nd0@gmail.com>
2026-07-18 03:22:09 +0900

    chore: アプリ構成に不要な生成物を削除する処理追加(#24)

--- scripts/fetch-articles.sh
+++ scripts/fetch-articles.sh
@@ -6,3 +6,8 @@ readonly TARGET_DIR="articles"
 
 rm -rf "$TARGET_DIR"
 git clone --depth 1 "$ARTICLES_REPO_URL" "$TARGET_DIR"
+
+# NOTE: アプリ構成に不要なので削除
+rm -rf "$TARGET_DIR/.git"
+rm -f "$TARGET_DIR/README.md"
+rm -rf "$TARGET_DIR/scripts"
