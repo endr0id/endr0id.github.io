19b9edc9202862b50e5847201e5e12ed7aaaa387
w4.3nd0 <w4.3nd0@gmail.com>
2026-07-20 00:42:54 +0900

    feat: 型定義追加(#24)

--- src/lib/mdx/getArticle.ts
+++ src/lib/mdx/getArticle.ts
@@ -1,11 +1,19 @@
+import type { ComponentType } from "react";
 import { readFile } from "node:fs/promises";
 import { ARTICLE_ROOT } from "@/src/constants";
+
+export interface ArticleData {
+  Component: ComponentType<any>;
+  markdown: string;
+}
 
 export async function getArticle(locale: string, slug: string) {
   const filePath = `${ARTICLE_ROOT}/${locale}/${slug}/index.mdx`;
   const markdown = await readFile(filePath, "utf-8");
 
-  const module = await import(`@/${ARTICLE_ROOT}/${locale}/${slug}/index.mdx`);
+  const module = (await import(
+    `@/${ARTICLE_ROOT}/${locale}/${slug}/index.mdx`
+  )) as { default: ComponentType<any> };
 
   return {
     Component: module.default,
