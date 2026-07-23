03b8d50da67a2914a48135d4609b135bb5d4da70
w4.3nd0 <w4.3nd0@gmail.com>
2026-07-24 01:18:20 +0900

    feat: asciiでタイトル表示(#24)

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
+import clsx from "clsx";
+import { headline } from "@/src/constants";
 import { getArticlesMetadata } from "@/src/lib/mdx/getArticlesMetadata";
 import Card from "./_components/card/Card";
 
+const asciiPreStyle = clsx([
+  // 等幅表示を保証
+  "select-none whitespace-pre",
+  // 等幅レンダリングの強化
+  "[font-variant-numeric:tabular-nums]",
+  // 行間とサイズ
+  "text-[12px] lg:text-[15px] leading-[120%]",
+]);
+
 export default async function Home() {
   const articles = await getArticlesMetadata("ja");
-
   return (
     <div className="px-10">
-      <p className="my-16 text-center sm:text-3xl md:text-5xl lg:text-7xl font-semibold">
-        Hello, world
-      </p>
+      <div className="my-16 text-center">
+        <pre className={asciiPreStyle}>{headline}</pre>
+      </div>
       <section>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {articles.map((article) => (
