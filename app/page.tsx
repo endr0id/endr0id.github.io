7ff35e8879b5205b543c0d5a98c9b49992740d7c
w4.3nd0 <w4.3nd0@gmail.com>
2026-07-28 02:47:53 +0900

    refactor: ヒーローセクションをセマンティック対応(25)

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,32 @@ import { hero } from "@/src/constants";
 import { getArticlesMetadata } from "@/src/lib/mdx/getArticlesMetadata";
 import Card from "./_components/card/Card";
 
-const asciiPreStyle = clsx([
-  "whitespace-pre",
-  "text-[12px] lg:text-[15px]",
-  "leading-none",
-  "tracking-[-0.05em]",
-]);
-
 export default async function Home() {
   const articles = await getArticlesMetadata("ja");
   return (
-    <div className="px-10">
-      <div className="flex flex-col gap-6 mt-16 mb-8 text-center">
-        <pre className={asciiPreStyle}>{hero.title}</pre>
-        <pre className="text-neutral-500 dark:text-neutral-400">
-          {hero.description}
-        </pre>
-      </div>
+    <div className="px-4 sm:px-6 lg:px-10">
+      <section className="mt-16 mb-8 text-center">
+        <h1>
+          <pre
+            className={clsx([
+              "whitespace-pre",
+              "text-[7px] sm:text-[12px] lg:text-[15px]",
+              "leading-none",
+              "tracking-[-0.05em]",
+              "aria-hidden=true",
+            ])}
+          >
+            {hero.title}
+          </pre>
+        </h1>
+        <p className="mt-4 text-neutral-500 dark:text-neutral-400">
+          Built. Broke. Experimented.
+          <span className="block sm:inline"> Made things work. Learned.</span>
+          <span className="block">
+            Sharing experiences from web development and programming.
+          </span>
+        </p>
+      </section>
       <section>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {articles.map((article) => (
