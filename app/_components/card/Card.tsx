2d4edf9ed86b2f601201436132fb25354a81ab61
w4.3nd0 <w4.3nd0@gmail.com>
2026-07-30 00:02:39 +0900

    fix: radix-uiのアスペクト比コンポーネント使用(#25)

--- app/_components/card/Card.tsx
+++ app/_components/card/Card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { _React } from "@dev.icons/react";
+import { AspectRatio } from "radix-ui";
 
 interface CardProps {
   path: string;
@@ -13,9 +13,9 @@ const Card = (props: CardProps) => {
   const { path, imagePath, title, description } = props;
 
   return (
-    <article className="w-full rounded-lg overflow-hidden">
+    <article className="w-full relative rounded-lg overflow-hidden">
       <Link href={path} className="flex flex-col gap-4">
-        <div className="relative aspect-3/2 w-full overflow-hidden rounded-lg">
+        <AspectRatio.Root ratio={3 / 2}>
           <Image
             src={imagePath}
             alt=""
@@ -24,7 +24,7 @@ const Card = (props: CardProps) => {
             className="object-cover"
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
-        </div>
+        </AspectRatio.Root>
         <div className="flex flex-col gap-2">
           <h3 className="font-semibold">{title}</h3>
           <p className="font-light text-neutral-500 dark:text-neutral-400">
