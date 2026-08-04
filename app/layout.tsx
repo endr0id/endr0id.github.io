4c600e8cc7b76da4dc83792def6f3d581b446b44
w4.3nd0 <w4.3nd0@gmail.com>
2026-08-04 23:10:59 +0900

    bugfix: html要素のlangを固定値に修正
    事象内容
    build時にapp/layout.tsxが動的パラメータを受け取る形式になっていたが動的パラメータは子ルートで適用されるため
    ビルドエラーになりデプロイ失敗した。
    
    パラメータの利用箇所がhtmlのlangだった為、暫定対応で固定値を入れて対応。
    
    またstorybookファイルに既に削除したライブラリが参照されていたためそちらも変更。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { defaultLocale } from "@/config/locale";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,16 +21,12 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
 }>) {
-  const { locale } = await params;
-
   return (
     <html
-      lang={locale}
+      lang={defaultLocale}
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       suppressHydrationWarning
     >
