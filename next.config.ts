aa632988b9ca14c7e27e9ef6af716a6fb731fb73
w4.3nd0 <w4.3nd0@gmail.com>
2026-08-04 20:51:37 +0900

    feat: mdxプラグインを認識させる(#22)

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,11 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
-    remarkPlugins: ["remark-frontmatter", "remark-mdx-frontmatter"],
+    remarkPlugins: [
+      "remark-frontmatter",
+      "remark-mdx-frontmatter",
+      "remark-gfm",
+    ],
   },
 });
 
