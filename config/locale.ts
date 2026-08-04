10364afacf134bea55da8ae7d8fa52a9c6289a26
w4.3nd0 <w4.3nd0@gmail.com>
2026-08-04 19:47:19 +0900

    fix: localeの順序変更(#13)

--- config/locale.ts
+++ config/locale.ts
@@ -1,9 +1,9 @@
-export const locales = ["en", "ja"] as const;
+export const locales = ["ja", "en"] as const;
 export type Locale = (typeof locales)[number];
 const supportedLocales: ReadonlySet<string> = new Set(locales);
 
 // NOTE: ネゴシエーション失敗時のfallback
-export const defaultLocale: Locale = locales[0];
+export const defaultLocale: Locale = locales[1];
 
 export const isLocale = (value: string): value is Locale => {
   return supportedLocales.has(value);
