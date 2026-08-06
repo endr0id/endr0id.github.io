42bca38319ba5d281ddd04dec7322279bc0cad4c
w4.3nd0 <w4.3nd0@gmail.com>
2026-08-06 22:33:58 +0900

    fix: サイズ調整(#48)

--- app/[locale]/me/page.tsx
+++ app/[locale]/me/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <main className="lg:w-[1280px] mx-auto">
       <div className="flex items-center gap-4">
-        <Avatar.Root className="inline-flex rounded-full overflow-hidden size-[45px]">
+        <Avatar.Root className="inline-flex rounded-full overflow-hidden size-[120px]">
           <Avatar.Fallback
             className={clsx([
               "flex items-center justify-center",
@@ -20,12 +20,12 @@ export default function Page() {
 
         <section aria-labelledby="profile">
           <h1 className="text-3xl font-bold tracking-tight">Wataru Endo</h1>
-          <p className="my-1 text-base text-neutral-500 dark:text-neutral-400">
+          <p className="mb-2 text-base text-neutral-500 dark:text-neutral-400">
             Software Engineer
           </p>
           <p
             className={clsx([
-              "max-w-[1280px] mt-3",
+              "w-full mt-3",
               "whitespace-pre-line leading-relaxed",
               "text-base text-neutral-600 dark:text-neutral-300",
             ])}
