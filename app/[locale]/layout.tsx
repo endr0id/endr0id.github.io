086ed7aedc3e8e71e2bc49527b5288d9e2850beb
w4.3nd0 <w4.3nd0@gmail.com>
2026-08-08 18:02:20 +0900

    bugfix: ビルドエラー解消
    layout.tsxのparamsを型をナローイング(params: Promise<{locale: Locale}>)したが
    Next.js側で自動生成した「params.localeはstring」と型が食い違った為、型を合わせた。
    
    Headerコンポーネントはlocaleをpropsで受け取る様にしたがその変更点が反映されていないことにより失敗

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
+import { notFound } from "next/navigation";
 import { ThemeProvider } from "@teispace/next-themes";
-import { type Locale, locales } from "@/config/locale";
+import { type Locale, isLocale, locales } from "@/config/locale";
 import Header from "./_components/header/Header";
 import ViewportFade from "./_components/viewportFade/ViewportFade";
 
@@ -11,10 +12,14 @@ export default async function Layout({
   params,
   children,
 }: Readonly<{
-  params: Promise<{ locale: Locale }>;
+  params: Promise<{ locale: string }>;
   children: React.ReactNode;
 }>) {
   const { locale } = await params;
+
+  if (!isLocale(locale)) {
+    return notFound();
+  }
 
   return (
     <ThemeProvider enableSystem={false}>
